274333fa99c23b580b3a389448355c6433b86a2b
Jalil <abduljalilzakaria1@gmail.com>
2025-07-12 06:25:14 +0200

    queries to implement indexes

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,43 @@
+
+
+
+#Index creation for users table
+
+CREATE INDEX idx_users_email ON users(email);
+CREATE INDEX idx_users_role ON users(role);
+
+
+#Index creation for bookings table
+
+CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+CREATE INDEX idx_bookings_status ON bookings(status);
+
+#Index creation for properties table
+
+CREATE INDEX idx_properties_host_id ON properties(host_id);
+CREATE INDEX idx_properties_location ON properties(location);
+CREATE INDEX idx_properties_pricepernight ON properties(pricepernight);
+
+
+
+
+#  Measure Before Index
+
+EXPLAIN SELECT *
+FROM bookings
+WHERE user_id = '11111111-1111-1111-1111-111111111111';
+
+# Add Index
+CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+
+# Measure After Index
+EXPLAIN SELECT *
+FROM bookings
+WHERE user_id = '11111111-1111-1111-1111-111111111111';
+
+# actual execution time and cost estimates
+EXPLAIN ANALYZE
+SELECT *
+FROM bookings
+WHERE user_id = '11111111-1111-1111-1111-111111111111';
